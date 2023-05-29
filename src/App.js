53f9364c4b48accdfa7aa7c489918c69aafadd73
Trần Thị Tứ Linh <tranthitulinh1305@gmail.com>
2023-05-29 14:45:38 +0700

    video banner

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, CardMedia, IconButton, Typography } from "@mui/material";
+import "./App.css";
+import { styled } from "@mui/material/styles";
+import React, { useState } from "react";
+const CoverButton = styled("div")(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  marginTop: theme.spacing(5),
+}));
 
 function App() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const getImageSource = () => {
+    return isHovered
+      ? "https://www.sony.com.sg/microsite/gofullframemirrorless/assets/images/revamp/videowithsony/landing-video-play-hover.png"
+      : "https://www.sony.com.sg/microsite/gofullframemirrorless/assets/images/revamp/videowithsony/landing-video-play.png";
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ position: "relative" }}>
+      <CardMedia
+        autoPlay
+        loop
+        muted
+        component="video"
+        src="https://www.sony.com.sg/microsite/gofullframemirrorless/videos/new-video-landing-bg-audio.mp4"
+      />
+      <Box
+        sx={{
+          background: "rgba(33, 38, 46,0.5)",
+          position: "absolute",
+          top: "0",
+          left: "0",
+          right: "0",
+          width: "100%",
+          height: "100%",
+        }}
+      />
+      <Box
+        sx={{
+          position: "absolute",
+          top: "30%",
+          left: "25%",
+          color: "white",
+          fontWeight: "bold",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h2">Bring your creative vision to life</Typography>
+        <CoverButton>
+          <IconButton component="div">
+            <img
+              alt="Hình ảnh"
+              src={getImageSource()}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            />
+            {/* <PlayCircleOutline fontSize="large" /> */}
+          </IconButton>
+        </CoverButton>
+        <CoverButton>
+          <IconButton component="div">
+            <img
+              alt="Hình ảnh"
+              src="https://www.sony.com.sg/microsite/gofullframemirrorless/assets/images/revamp/videowithsony/landing-arrow-down.png"
+            />
+          </IconButton>
+        </CoverButton>
+      </Box>
+    </Box>
   );
 }
 
---
+++ src/tmp.js
@@ -0,0 +1,53 @@
+// {
+//   /* <Box sx={{ position: "relative" }}>
+//         <Card>
+//           <Box
+//             sx={{
+//               width: "100%",
+//               height: "100%",
+//             }}
+//           >
+//             <CardMedia
+//               autoPlay
+//               loop
+//               muted
+//               component="video"
+//               src="https://www.sony.com.sg/microsite/gofullframemirrorless/videos/new-video-landing-bg-audio.mp4"
+//             />
+//           </Box>
+
+//           <Box
+//             sx={{
+//               background: "rgba(33, 38, 46,0.5)",
+//               position: "absolute",
+//               top: "0",
+//               left: "0",
+//               right: "0",
+//               width: "100%",
+//               height: "100%",
+//             }}
+//           >
+//             <CardContent
+//               sx={{
+//                 position: "absolute",
+//                 top: "50%",
+//                 left: "25%",
+//                 color: "white",
+//                 fontWeight: "bold",
+//                 textAlign: "center",
+//               }}
+//             >
+//               <Typography
+//                 variant="h2"
+//                 // textAlign="center"
+//                 // zIndex={1}
+//                 marginTop={0}
+//                 marginBottom={10}
+//               >
+//                 Bring your creative vision to life
+//               </Typography>
+//             </CardContent>
+//           </Box>
+//         </Card>
+//       </Box> */
+// }
